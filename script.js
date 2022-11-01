353ade797412f7fe89e01944757e6650efd10a42
jebrong <92987206+jebrong@users.noreply.github.com>
2022-11-01 17:27:33 +0800

    working slding options

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const menuBar = document.querySelector("#nav-icon");
+const navBar = document.querySelector(".navbar");
 
 menuBar.addEventListener("click", () => {
   menuBar.classList.toggle("open");
+  navBar.classList.toggle("open");
+  navBar.classList.toggle("close");
 });
